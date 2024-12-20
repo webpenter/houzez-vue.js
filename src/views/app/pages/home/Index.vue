8fd752f137cbc71c741749bdd9399a3466a81bc4
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2024-12-21 02:16:42 +0500

    add property tabs in home

--- src/views/app/pages/home/Index.vue
+++ src/views/app/pages/home/Index.vue
@@ -1,9 +1,11 @@
 <template>
     <ParallaxBannerWithTabs/>
     <ModulePropertyCardsV1/>
+    <ModulePropertyTabsV1/>
 </template>
 
 <script setup>
 import ParallaxBannerWithTabs from '../../inc/header/ParallaxBannerWithTabs.vue';
 import ModulePropertyCardsV1 from './ModulePropertyCardsV1.vue';
+import ModulePropertyTabsV1 from './ModulePropertyTabsV1.vue';
 </script>
