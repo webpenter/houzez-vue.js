625b19398926b42ecca3de53e3a8f0bcb5691d83
Husnain81 <163633770+Husnain81@users.noreply.github.com>
2024-12-20 11:42:23 +0500

    files included

---
+++ src/views/app/inc/property/PropertyNearbyBlock.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="property-nearby-wrap property-section-wrap" id="property-nearby-wrap">
+	<div class="block-wrap">
+		<div class="block-title-wrap d-flex justify-content-between align-items-center">
+			<h2>What's Nearby?</h2>
+			<div class="small-text grey nearby-logo">Powered by <i class="houzez-icon icon-social-media-yelp"></i> <strong>Yelp</strong></div>
+		</div><!-- block-title-wrap -->
+		<div class="block-content-wrap">
+
+			<div class="what-nearby">
+				<dl>
+					<dt>Education</dt>
+					<dd>
+						<div class="what-nearby-left">
+							Spanish Classes &amp; Lessons <span class="time-review"> (0.97 mi) </span>
+						</div>
+						<div class="what-nearby-right">
+							<div class="rating-wrap">
+								<div class="rating-container">
+									<div class="rating star">                                            
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating half-star"></span>
+										<span class="icon-rating empty-star"></span>
+										<span class="label bg-success">Exceptional</span>
+										<span class="time-review">5 reviews</span>
+									</div> 
+								</div>
+							</div>
+						</div>
+					</dd>
+					<dd>
+						<div class="what-nearby-left">
+							IGA Culinary School <span class="time-review"> (0.97 mi) </span>
+						</div>
+						<div class="what-nearby-right">
+							<div class="rating-wrap">
+								<div class="rating-container">
+									<div class="rating star">                                            
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating half-star"></span>
+										<span class="icon-rating empty-star"></span>
+										<span class="label bg-success">Exceptional</span>
+										<span class="time-review">5 reviews</span>
+									</div> 
+								</div>
+							</div>
+						</div>
+					</dd>
+					<dd>
+						<div class="what-nearby-left">
+							Gold Coast Schools <span class="time-review"> (0.97 mi) </span>
+						</div>
+						<div class="what-nearby-right">
+							<div class="rating-wrap">
+								<div class="rating-container">
+									<div class="rating star">                                            
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating half-star"></span>
+										<span class="icon-rating empty-star"></span>
+										<span class="label bg-success">Exceptional</span>
+										<span class="time-review">5 reviews</span>
+									</div> 
+								</div>
+							</div>
+						</div>
+					</dd>
+				</dl>
+				<dl>
+					<dt>Food</dt>
+					<dd>
+						<div class="what-nearby-left">
+							The Carrot  <span class="time-review"> (0.97 mi) </span>
+						</div>
+						<div class="what-nearby-right">
+							<div class="rating-wrap">
+								<div class="rating-container">
+									<div class="rating star">                                            
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating half-star"></span>
+										<span class="icon-rating empty-star"></span>
+										<span class="label bg-success">Exceptional</span>
+										<span class="time-review">5 reviews</span>
+									</div> 
+								</div>
+							</div>
+						</div>
+					</dd>
+					<dd>
+						<div class="what-nearby-left">
+							Top Notch Bistro <span class="time-review"> (0.97 mi) </span>
+						</div>
+						<div class="what-nearby-right">
+							<div class="rating-wrap">
+								<div class="rating-container">
+									<div class="rating star">                                            
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating half-star"></span>
+										<span class="icon-rating empty-star"></span>
+										<span class="label bg-success">Exceptional</span>
+										<span class="time-review">5 reviews</span>
+									</div> 
+								</div>
+							</div>
+						</div>
+					</dd>
+					<dd>
+						<div class="what-nearby-left">
+							Paon Eatery <span class="time-review"> (0.97 mi) </span>
+						</div>
+						<div class="what-nearby-right">
+							<div class="rating-wrap">
+								<div class="rating-container">
+									<div class="rating star">                                            
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating full-star"></span>
+										<span class="icon-rating half-star"></span>
+										<span class="icon-rating empty-star"></span>
+										<span class="label bg-success">Exceptional</span>
+										<span class="time-review">5 reviews</span>
+									</div> 
+								</div>
+							</div>
+						</div>
+					</dd>
+				</dl>
+			</div>
+
+
+		</div><!-- block-content-wrap -->
+	</div><!-- block-wrap -->
+</div><!-- property-walkscore-wrap -->
+</template>
+<script setup>
+</script>
