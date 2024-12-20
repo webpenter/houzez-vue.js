57bd88c4f0aa09cd31f47544a2d62e196a8d81c2
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2024-12-21 03:08:24 +0500

    add taxonomy sextion in home page

--- src/views/app/pages/home/Index.vue
+++ src/views/app/pages/home/Index.vue
@@ -1,11 +1,15 @@
 <template>
     <ParallaxBannerWithTabs/>
     <ModulePropertyCardsV1/>
+    <div style="margin-block: 60px;"></div>
     <ModulePropertyTabsV1/>
+    <ModuleTaxonomyGrids/>
+    <div style="margin-block: 60px;"></div>
 </template>
 
 <script setup>
 import ParallaxBannerWithTabs from '../../inc/header/ParallaxBannerWithTabs.vue';
 import ModulePropertyCardsV1 from './ModulePropertyCardsV1.vue';
 import ModulePropertyTabsV1 from './ModulePropertyTabsV1.vue';
+import ModuleTaxonomyGrids from './ModuleTaxonomyGrids.vue';
 </script>
