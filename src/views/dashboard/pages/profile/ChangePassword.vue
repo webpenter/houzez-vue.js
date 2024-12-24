ffd4aa8977b78b446c6ad424915539c383168d3c
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2024-12-24 17:43:46 +0500

    add profile page

---
+++ src/views/dashboard/pages/profile/ChangePassword.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="dashboard-content-block">
+                        <div class="row">
+                            <div class="col-md-3 col-sm-12">
+                                <h2>Change Password</h2>
+                            </div><!-- col-md-3 col-sm-12 -->
+
+                            <div class="col-md-9 col-sm-12">
+                                <div class="row">
+                                    <div class="col-md-12 col-sm-12">
+                                        <div class="form-group">
+                                            <label>Old Password</label>
+                                            <input class="form-control" name="name" placeholder="Enter your current password" type="text">
+                                        </div>
+                                    </div><!-- col-md-12 col-sm-12 -->
+                                    <div class="col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <label>New Password</label>
+                                            <input class="form-control" name="name" placeholder="Enter your new password" type="text">
+                                        </div>
+                                    </div><!-- col-md-6 col-sm-12 -->
+                                    <div class="col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <label>Confirm New Password</label>
+                                            <input class="form-control" name="name" placeholder="Enter your new password again" type="text">
+                                        </div>
+                                    </div><!-- col-md-6 col-sm-12 -->
+                                </div><!-- row -->
+                                <button class="btn btn-success">Update Password</button>
+                            </div><!-- col-md-9 col-sm-12 -->
+                        </div><!-- row -->
+                    </div><!-- dashboard-content-block -->
+</template>
+
+<script setup></script>
