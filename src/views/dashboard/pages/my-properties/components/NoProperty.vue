77be04f98284630a2e9936d1b4f5f4dfaf7468bd
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2024-12-26 17:18:03 +0500

    create properties page

---
+++ src/views/dashboard/pages/my-properties/components/NoProperty.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="dashboard-content-block">
+        You don't have any property listed. 
+        <RouterLink :to="{name:'dashboard.create-listing'}">
+            <strong>Create a listing</strong>
+        </RouterLink>
+    </div><!-- dashboard-content-block -->
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
