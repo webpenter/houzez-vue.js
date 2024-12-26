c9b56056d3e952f0c05db15c6cda19dd74cf9ba6
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2024-12-26 15:05:55 +0500

    create board pages

--- src/router/index.js
+++ src/router/index.js
@@ -19,49 +19,37 @@ const router = createRouter({
             path: '/',
             name:'app.home',
             component:() => import('@/views/app/pages/home/Index.vue'),
-            meta:{
-              title:'Home'
-            }
+            meta:{ title:'Home' }
           },
           {
             path: '/add-new-property',
             name:'app.add-new-property',
             component:() => import('@/views/app/pages/add-new-property/Index.vue'),
-            meta:{
-              title:'Add Property'
-            }
+            meta:{ title:'Add Property' }
           },
           {
             path: '/properties',
             name:'app.properties',
             component:() => import('@/views/app/pages/properties/Index.vue'),
-            meta:{
-              title:'Properties'
-            }
+            meta:{ title:'Properties' }
           },
           {
             path: '/property',
             name:'app.property',
             component:() => import('@/views/app/pages/property/Index.vue'),
-            meta:{
-              title:'Property'
-            }
+            meta:{ title:'Property' }
           },
           {
             path: '/realtor',
             name:'app.realtor',
             component:() => import('@/views/app/pages/realtor/Index.vue'),
-            meta:{
-              title:'Realtor'
-            }
+            meta:{ title:'Realtor' }
           },
           {
             path: '/search-results',
             name:'app.search-results',
             component:() => import('@/views/app/pages/search-results/Index.vue'),
-            meta:{
-              title:'Search Results'
-            }
+            meta:{ title:'Search Results' }
           },
       ]
     },
@@ -79,76 +67,76 @@ const router = createRouter({
       component:() => import('@/views/dashboard/layout/Index.vue'),
       children:[
           {
-            path: '/dashboard/board',
-            name:'dashboard.board',
-            component:() => import('@/views/dashboard/pages/board/Index.vue'),
-            meta:{
-              title:'Board'
-            }
+            path: '/dashboard/crm',
+            name:'dashboard.crm',
+            component:() => import('@/views/dashboard/pages/board/crm/Index.vue'),
+            meta:{ title:'Activities' }
+          },
+          {
+            path: '/dashboard/crm-active-deals',
+            name:'dashboard.crm-active-deals',
+            component:() => import('@/views/dashboard/pages/board/crm-active-deals/Index.vue'),
+            meta:{ title:'Deals' }
+          },
+          {
+            path: '/dashboard/crm-enquiries',
+            name:'dashboard.crm-enquiries',
+            component:() => import('@/views/dashboard/pages/board/crm-enquiries/Index.vue'),
+            meta:{ title:'Enquiries' }
+          },
+          {
+            path: '/dashboard/crm-leads',
+            name:'dashboard.crm-leads',
+            component:() => import('@/views/dashboard/pages/board/crm-leads/Index.vue'),
+            meta:{ title:'Leads' }
           },
           {
             path: '/dashboard/insight',
             name:'dashboard.insight',
             component:() => import('@/views/dashboard/pages/insight/Index.vue'),
-            meta:{
-              title:'Insight'
-            }
+            meta:{ title:'Insight' }
           },
           {
             path: '/dashboard/my-properties',
             name:'dashboard.my-properties',
             component:() => import('@/views/dashboard/pages/my-properties/Index.vue'),
-            meta:{
-              title:'My Properties'
-            }
+            meta:{ title:'My Properties' }
           },
           {
             path: '/dashboard/create-listing',
             name:'dashboard.create-listing',
             component:() => import('@/views/dashboard/pages/create-listing/Index.vue'),
-            meta:{
-              title:'Create Listing'
-            }
+            meta:{ title:'Create Listing' }
           },
           {
             path: '/dashboard/favorite-properties',
             name:'dashboard.favorite-properties',
             component:() => import('@/views/dashboard/pages/favorite-properties/Index.vue'),
-            meta:{
-              title:'Favorite Properties'
-            }
+            meta:{ title:'Favorite Properties' }
           },
           {
             path: '/dashboard/saved-searches',
             name:'dashboard.saved-searches',
             component:() => import('@/views/dashboard/pages/saved-searches/Index.vue'),
-            meta:{
-              title:'Saved Searches'
-            }
+            meta:{ title:'Saved Searches' }
           },
           {
             path: '/dashboard/invoices',
             name:'dashboard.invoices',
             component:() => import('@/views/dashboard/pages/invoices/Index.vue'),
-            meta:{
-              title:'Invoices'
-            }
+            meta:{ title:'Invoices' }
           },
           {
             path: '/dashboard/messages',
             name:'dashboard.messages',
             component:() => import('@/views/dashboard/pages/messages/Index.vue'),
-            meta:{
-              title:'Messages'
-            }
+            meta:{ title:'Messages' }
           },
           {
             path: '/dashboard/my-profile',
             name:'dashboard.my-profile',
             component:() => import('@/views/dashboard/pages/my-profile/Index.vue'),
-            meta:{
-              title:'My Profile'
-            }
+            meta:{ title:'My Profile' }
           },
       ]
     },
--- src/views/inc/dashboard/AgentMenu.vue
+++ src/views/inc/dashboard/AgentMenu.vue
@@ -2,13 +2,32 @@
     <ul class="side-menu list-unstyled">
 
 		<template v-for="route in routes" :key="route.id">
-			<li class="side-menu-item">
+				<li class="side-menu-item" :class="{'side-menu-parent-selected':route.sub.length > 0}">
+					<RouterLink :to="{name:route.name}" active-class="active">
+						<i class="houzez-icon mr-2" :class="route.icon"></i>
+						{{ route.title }}
+					</RouterLink>
+					<template v-if="route.sub.length > 0">
+							<ul class="side-menu-dropdown list-unstyled">
+								<template v-for="sub_route in route.sub" :key="sub_route.id">
+									<li class="side-menu-item">
+										<RouterLink :to="{name:sub_route.name}" active-class="active">
+											<i class="houzez-icon icon-arrow-right-1"></i>
+											{{ sub_route.title }}
+										</RouterLink>
+									</li>
+								</template>
+							</ul>
+					</template>
+					<span v-if="route.title == 'Messages'" class="notification-circle"></span>
+				</li>
+			<!-- <li class="side-menu-item">
 				<RouterLink :to="{name:route.name}" active-class="active">
 					<i class="houzez-icon mr-2" :class="route.icon"></i>
 					{{ route.title }}
 				</RouterLink>
 				<span v-if="route.title == 'Messages'" class="notification-circle"></span>
-			</li>
+			</li> -->
 		</template>
 		
 		<li class="side-menu-item">
@@ -25,15 +44,40 @@ import { RouterLink } from 'vue-router';
 
 // Dashboard Route List
 const routes = [
-	{ id:1, title:'Board', name:'dashboard.board', icon:'icon-layout-dashboard' },
-	{ id:2, title:'Insight', name:'dashboard.insight', icon:'icon-analytics-bars' },
-	{ id:3, title:'My Properties', name:'dashboard.my-properties', icon:'icon-building-cloudy' },
-	{ id:4, title:'Create Listing', name:'dashboard.create-listing', icon:'icon-add-circle' },
-	{ id:5, title:'Favorites', name:'dashboard.favorite-properties', icon:'icon-love-it' },
-	{ id:6, title:'Saves Searches', name:'dashboard.saved-searches', icon:'icon-search' },
-	{ id:7, title:'Invoices', name:'dashboard.invoices', icon:'icon-accounting-document' },
-	{ id:8, title:'Messages', name:'dashboard.messages', icon:'icon-messages-bubble' },
-	{ id:9, title:'My Profile', name:'dashboard.my-profile', icon:'icon-single-neutral-circle' },
+	{ 
+		id:1,
+		title:'Board',
+		name:'dashboard.crm',
+		icon:'icon-layout-dashboard',
+		sub:[
+			{id:1, title:'Activities', name:'dashboard.crm'},
+			{id:2, title:'Deals', name:'dashboard.crm-active-deals'},
+			{id:3, title:'Leads', name:'dashboard.crm-leads'},
+			{id:4, title:'Enquiries', name:'dashboard.crm-enquiries'},
+		] 
+	},
+	{ id:2, title:'Insight', name:'dashboard.insight', icon:'icon-analytics-bars', sub:[] },
+	{ 
+		id:3,
+		title:'My Properties',
+		name:'dashboard.my-properties',
+		icon:'icon-building-cloudy',
+		sub:[
+			// {id:1, title:'All', name:'dashboard.'},
+			// {id:2, title:'Published', name:'dashboard.'},
+			// {id:3, title:'Pending', name:'dashboard.'},
+			// {id:4, title:'Expired', name:'dashboard.'},
+			// {id:5, title:'Draft', name:'dashboard.'},
+			// {id:6, title:'On Hold', name:'dashboard.'},
+			// {id:7, title:'Disapproved', name:'dashboard.'},
+		] 
+	},
+	{ id:4, title:'Create Listing', name:'dashboard.create-listing', icon:'icon-add-circle', sub:[] },
+	{ id:5, title:'Favorites', name:'dashboard.favorite-properties', icon:'icon-love-it', sub:[] },
+	{ id:6, title:'Saved Searches', name:'dashboard.saved-searches', icon:'icon-search', sub:[] },
+	{ id:7, title:'Invoices', name:'dashboard.invoices', icon:'icon-accounting-document', sub:[] },
+	{ id:8, title:'Messages', name:'dashboard.messages', icon:'icon-messages-bubble', sub:[] },
+	{ id:9, title:'My Profile', name:'dashboard.my-profile', icon:'icon-single-neutral-circle', sub:[] },
 ];
 </script>
 
