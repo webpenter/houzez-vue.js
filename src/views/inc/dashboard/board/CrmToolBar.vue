76d41f6744fa59306d34e160eefd7b1b874ddd5b
Naraish Kumar <rajnaresh329@gmail.com>
2024-12-27 13:11:18 +0500

    crm code pushed

---
+++ src/views/inc/dashboard/board/CrmToolBar.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="alert alert-success" role="alert">
+    Settings saved successfully.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+    </button>
+</div>
+<div class="alert alert-danger" role="alert">
+    Something went wrong.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+    </button>
+</div>
+<div class="dashboard-tool-block">
+    <div class="dashboard-tool-buttons-block">
+        <div class="dashboard-tool-button">
+            <button onclick="window.location.href='dashboard-import-csv.php';" class="btn btn-primary-outlined">Import</button>
+        </div>
+        <div class="dashboard-tool-button">
+            <button class="btn btn-primary-outlined">Export</button>
+        </div>
+        <div class="dashboard-tool-button">
+            <button id="btn-delete" class="btn btn-grey-outlined">Delete</button>
+        </div>
+        <div class="dashboard-tool-button">
+            <div class="btn"><i class="houzez-icon icon-single-neutral-circle mr-2 grey"></i>36 Records</div>
+        </div>    
+    </div><!-- dashboard-tool-buttons-block -->
+    
+    <div class="dashboard-tool-search-block">
+        <div class="dashboard-crm-search-wrap">
+            <div class="d-flex">
+                <div class="flex-grow-1">
+                    <div class="dashboard-crm-search">
+                        <div class="d-flex">
+                            <div class="form-group">
+                                <div class="search-icon">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                </div><!-- search-icon -->
+                            </div><!-- form-group -->
+                            <button type="submit" class="btn btn-search btn-secondary">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- dashboard-crm-search-wrap -->
+    </div><!-- dashboard-tool-search-block -->
+    
+</div><!-- dashboard-tool-block -->
+
+
+</template>
+
+<script setup>
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var checkbox = document.getElementById("checkbox-delete");
+        var button = document.getElementById("btn-delete");
+
+        checkbox.addEventListener("change", function() {
+            if (checkbox.checked) {
+                button.classList.remove("btn-grey-outlined");
+                button.classList.add("btn-primary");
+            } else {
+                button.classList.remove("btn-primary");
+                button.classList.add("btn-grey-outlined");
+            }
+        });
+    });
+
+</script>
