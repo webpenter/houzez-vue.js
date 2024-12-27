73ac3e1f756c182473e1e12e84b7e48ab1231e39
Husnain81 <163633770+Husnain81@users.noreply.github.com>
2024-12-27 15:12:13 +0500

    messages work completed

---
+++ src/views/inc/dashboard/MessageTableItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <tr class="table-new">
+	<td data-label="From" class="table-nowrap">
+		<img class="rounded-circle" src="https://placehold.co/40x40" alt="agent">
+		<span class="ml-2">John Doe</span>
+	</td>
+	<td data-label="Property" class="table-nowrap">
+		<a href="#"><strong>Apartment With Ocean View</strong></a>
+	</td>
+	<!-- <td class="property-table-id" data-label="ID">HZ-001</td> -->
+	<td data-label="Last Message">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</td>
+	<td data-label="Date" class="table-nowrap">
+		March 19, 2019<br>
+		5:36 pm
+	</td>
+	<td data-label="Actions">
+		<div class="dropdown property-action-menu">
+			<button class="btn btn-primary-outlined dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Actions
+			</button>
+			<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+				<a class="dropdown-item" href="dashboard-agent-message.php">Reply</a>
+				<a class="dropdown-item" href="#modal-delete" data-toggle="modal">Delete</a>
+			</div>
+		</div>
+	</td>
+</tr>
+
+<tr>
+	<td data-label="From" class="table-nowrap">
+		<img class="rounded-circle" src="https://placehold.co/40x40" alt="agent">
+		<span class="ml-2">John Doe</span>
+	</td>
+	<td data-label="Property" class="table-nowrap">
+		<a href="#"><strong>Apartment With Ocean View</strong></a>
+	</td>
+	<!-- <td class="property-table-id" data-label="ID">HZ-001</td> -->
+	<td data-label="Last Message">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</td>
+	<td data-label="Date" class="table-nowrap">
+		March 19, 2019<br>
+		5:36 pm
+	</td>
+	<td data-label="Actions">
+		<div class="dropdown property-action-menu">
+			<button class="btn btn-primary-outlined dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Actions
+			</button>
+			<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+				<a class="dropdown-item" href="dashboard-agent-message.php">Reply</a>
+				<a class="dropdown-item" href="#modal-delete" data-toggle="modal">Delete</a>
+			</div>
+		</div>
+	</td>
+</tr>
+</template>
+<script setup>
+
+</script>
