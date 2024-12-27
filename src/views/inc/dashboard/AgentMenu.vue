b983af89c6d971c5ed18f9245e8cf602785c9dff
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2024-12-27 12:02:46 +0500

    add route list

--- src/views/inc/dashboard/AgentMenu.vue
+++ src/views/inc/dashboard/AgentMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="side-menu list-unstyled">
 
-		<template v-for="route in routes" :key="route.id">
+		<template v-for="route in DASHBOARD_ROUTES" :key="route.id">
 				<li class="side-menu-item" :class="{'side-menu-parent-selected':route.sub.length > 0}">
 					<RouterLink :to="{name:route.name}" active-class="active">
 						<i class="houzez-icon mr-2" :class="route.icon"></i>
@@ -11,7 +11,7 @@
 							<ul class="side-menu-dropdown list-unstyled">
 								<template v-for="sub_route in route.sub" :key="sub_route.id">
 									<li class="side-menu-item">
-										<RouterLink :to="{name:sub_route.name}" active-class="active">
+										<RouterLink :to="{name:sub_route.name}" active-class="active" >
 											<i class="houzez-icon icon-arrow-right-1"></i>
 											{{ sub_route.title }}
 										</RouterLink>
@@ -35,41 +35,11 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 
-// Dashboard Route List
-const routes = [
-	{ 
-		id:1,
-		title:'Board',
-		name:'dashboard.crm',
-		icon:'icon-layout-dashboard',
-		sub:[
-			{id:1, title:'Activities', name:'dashboard.crm'},
-			{id:2, title:'Deals', name:'dashboard.crm-active-deals'},
-			{id:3, title:'Leads', name:'dashboard.crm-leads'},
-			{id:4, title:'Enquiries', name:'dashboard.crm-enquiries'},
-		] 
-	},
-	{ id:2, title:'Insight', name:'dashboard.insight', icon:'icon-analytics-bars', sub:[] },
-	{ 
-		id:3,
-		title:'My Properties',
-		name:'dashboard.my-properties',
-		icon:'icon-building-cloudy',
-		sub:[
-			{id:1, title:'All', name:'dashboard.my-properties'},
-			{id:2, title:'Published', name:'dashboard.my-properties.published'},
-			{id:3, title:'Pending', name:'dashboard.my-properties.pending'},
-			{id:4, title:'Expired', name:'dashboard.my-properties.expired'},
-			{id:5, title:'Draft', name:'dashboard.my-properties.draft'},
-			{id:6, title:'On Hold', name:'dashboard.my-properties.hold'},
-			{id:7, title:'Disapproved', name:'dashboard.my-properties.disapproved'},
-		] 
-	},
-	{ id:4, title:'Create Listing', name:'dashboard.create-listing', icon:'icon-add-circle', sub:[] },
-	{ id:5, title:'Favorites', name:'dashboard.favorite-properties', icon:'icon-love-it', sub:[] },
-	{ id:6, title:'Saved Searches', name:'dashboard.saved-searches', icon:'icon-search', sub:[] },
-	{ id:7, title:'Invoices', name:'dashboard.invoices', icon:'icon-accounting-document', sub:[] },
-	{ id:8, title:'Messages', name:'dashboard.messages', icon:'icon-messages-bubble', sub:[] },
-	{ id:9, title:'My Profile', name:'dashboard.my-profile', icon:'icon-single-neutral-circle', sub:[] },
-];
+import { DASHBOARD_ROUTES } from '@/constants/routeList';
 </script>
+
+<style scoped>
+ .side-menu-dropdown a {
+	color: #839EB2;
+}
+</style>
