4b2da3580b0d2bd035f200a8d25d8488bf1320ad
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-01-15 02:41:10 +0500

    make social media accounts dynamic

--- src/stores/auth/profile.js
+++ src/stores/auth/profile.js
@@ -16,6 +16,7 @@ export const useProfile = defineStore("userProfile", {
     state: () => ({
         profile: {},
         profilePicture:"",
+        socialMedia:{},
         errors: null,
         baseUrl:'/profile'
     }),
@@ -36,7 +37,7 @@ export const useProfile = defineStore("userProfile", {
                 const url = `${this.baseUrl}/get-information`;
                 const res = await axiosInstance.get(url);
 
-                this.setProfile(res.data.data);
+                this.setProfile(res.data.profile);
 
                 return new Promise(resolve => {
                     return resolve(res);
@@ -51,6 +52,37 @@ export const useProfile = defineStore("userProfile", {
                 })
             }
         },
+
+        /**
+         * @feature Updates the user's profile information by sending a POST request to the server.
+         * @param {FormData} formData - The profile information data to be uploaded.
+         * @returns {Promise<void>} - Resolves when the profile information is successfully updated.Rejects with the server's error response if the update fails.
+         */
+        async updateProfileInfo(formData)  {
+            try {
+                const url = `${this.baseUrl}/update-information`;
+                const res = await axiosInstance.post(url, formData);
+
+                this.setProfile(res.data.profile);
+
+                return new Promise(resolve => {
+                    return  resolve(res);
+                })
+            } catch (error) {
+                if (error.response.data) {
+                    this.errors = error.response;
+                }
+
+                return new Promise(reject => {
+                    return reject(error.response);
+                })
+            }
+        },
+
+        /**
+         * @feature Fetch the authenticated user's profile picture from the server.
+         * @returns {Promise<Object>} A promise that resolves with the server response containing the profile picture data.Rejects with the error response in case of failure.
+         */
         async getProfilePicture(){
             try {
                 const res = await axiosInstance.get(`${this.baseUrl}/get-picture`);
@@ -84,10 +116,61 @@ export const useProfile = defineStore("userProfile", {
                     },
                 });
 
-                this.setProfile(res.data.profile_picture_url);
+                this.setProfilePicture(res.data.profile_picture_url);
 
                 return new Promise(resolve => {
-                    resolve(res);
+                    return  resolve(res);
+                })
+            } catch (error) {
+                if (error.response.data) {
+                    this.errors = error.response;
+                }
+
+                return new Promise(reject => {
+                    return reject(error.response);
+                })
+            }
+        },
+
+        /**
+         * @feature the user's social media information from the server.
+         * @returns {Promise<Object>} - Resolves with the server's response containing the user's social media data. Rejects with the server's error response if the request fails.
+         */
+        async getSocialMedia () {
+            try {
+                const url = `${this.baseUrl}/get-social-media`;
+                const res = await axiosInstance.get(url);
+
+                this.setSocialMedia(res.data.profile);
+
+                return new Promise(resolve => {
+                    return resolve(res);
+                })
+            } catch (error) {
+                if (error.response && error.response.data) {
+                    this.errors = error.response;
+                }
+
+                return new Promise(reject => {
+                    return reject(error.response);
+                })
+            }
+        },
+
+        /**
+         * @feature Updates the user's social media information by sending a POST request to the server.
+         * @param {FormData} formData - The social media information data to be uploaded.
+         * @returns {Promise<void>} - Resolves when the social media information is successfully updated. Rejects with the server's error response if the update fails.
+         */
+        async updateSocialMedia(formData)  {
+            try {
+                const url = `${this.baseUrl}/update-social-media`;
+                const res = await axiosInstance.post(url, formData);
+
+                this.setSocialMedia(res.data.profile);
+
+                return new Promise(resolve => {
+                    return  resolve(res);
                 })
             } catch (error) {
                 if (error.response.data) {
@@ -105,6 +188,9 @@ export const useProfile = defineStore("userProfile", {
         },
         setProfilePicture(data) {
             this.profilePicture = data;
+        },
+        setSocialMedia(data) {
+            this.socialMedia = data;
         },
 
     }
--- src/views/dashboard/pages/my-profile/Information.vue
+++ src/views/dashboard/pages/my-profile/Information.vue
@@ -1,5 +1,4 @@
 <template>
-  {{ profile }}
     <h2>Information</h2>
     <div class="dashboard-content-block">
                         <div class="row">
@@ -19,105 +18,107 @@
                             </div><!-- col-md-3 col-sm-12 -->
 
                             <div class="col-md-9 col-sm-12">
-                                <div class="row">
+                                <form @submit.prevent="submitForm">
+                                  <div class="row">
                                     <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Username</label>
-                                            <input class="form-control" v-model="formData.username" placeholder="Enter your username" type="text" disabled>
-                                        </div>
-                                    </div><!-- col-md-6 col-sm-12 -->
-                                    <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Email</label>
-                                            <input class="form-control" v-model="formData.email" placeholder="Enter your email" type="text">
-                                        </div>
+                                      <div class="form-group">
+                                        <label>Email</label>
+                                        <input class="form-control" v-model="formData.email" placeholder="Enter your email" type="text" readonly />
+                                      </div>
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>First Name</label>
-                                            <input class="form-control" v-model="formData.first_name" placeholder="Enter your first name" type="text">
-                                        </div>
+                                      <div class="form-group">
+                                        <label>Username</label>
+                                        <input class="form-control" v-model="formData.username" placeholder="Enter your username" type="text">
+                                      </div>
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Last Name</label>
-                                            <input class="form-control" v-model="formData.last_name" placeholder="Enter your last name" type="text">
-                                        </div>
-                                    </div><!-- col-md-6 col-sm-12 -->
-                                  <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Public Name</label>
-                                            <input class="form-control" v-model="formData.public_name" placeholder="Enter your last name" type="text">
-                                        </div>
+                                      <div class="form-group">
+                                        <label>First Name</label>
+                                        <input class="form-control" v-model="formData.first_name" placeholder="Enter your first name" type="text">
+                                      </div>
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Title</label>
-                                            <input class="form-control" v-model="formData.title" placeholder="Enter your title" type="text">
-                                        </div>
-                                    </div><!-- col-md-6 col-sm-12 -->
-                                  <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Position</label>
-                                            <input class="form-control" v-model="formData.position" placeholder="Enter your title" type="text">
-                                        </div>
+                                      <div class="form-group">
+                                        <label>Last Name</label>
+                                        <input class="form-control" v-model="formData.last_name" placeholder="Enter your last name" type="text">
+                                      </div>
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>License</label>
-                                            <input class="form-control" v-model="formData.license" placeholder="Enter your license" type="text">
-                                        </div>
+                                      <div class="form-group">
+                                        <label>Public Name</label>
+                                        <input class="form-control" v-model="formData.public_name" placeholder="Enter your last name" type="text">
+                                      </div>
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Tax Number</label>
-                                            <input class="form-control" v-model="formData.tax_number" placeholder="Enter your tax number" type="text">
-                                        </div>
+                                      <div class="form-group">
+                                        <label>Title</label>
+                                        <input class="form-control" v-model="formData.title" placeholder="Enter your title" type="text">
+                                      </div>
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Mobile</label>
-                                            <input class="form-control" v-model="formData.mobile" placeholder="Enter your phone number" type="text">
-                                        </div>
-                                    </div><!-- col-md-6 col-sm-12 -->
-                                  <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Phone</label>
-                                            <input class="form-control" v-model="formData.phone" placeholder="Enter your phone number" type="text">
-                                        </div>
-                                    </div><!-- col-md-6 col-sm-12 -->
-                                  <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Whatsapp</label>
-                                            <input class="form-control" v-model="formData.whatsapp" placeholder="Enter your phone number" type="text">
-                                        </div>
+                                      <div class="form-group">
+                                        <label>Position</label>
+                                        <input class="form-control" v-model="formData.position" placeholder="Enter your title" type="text">
+                                      </div>
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Company Name</label>
-                                            <input class="form-control" v-model="formData.company_name" placeholder="Enter your company name" type="text">
-                                        </div><!-- form-group -->
+                                      <div class="form-group">
+                                        <label>License</label>
+                                        <input class="form-control" v-model="formData.license" placeholder="Enter your license" type="text">
+                                      </div>
                                     </div><!-- col-md-6 col-sm-12 -->
-                                  <div class="col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Service area</label>
-                                            <input class="form-control" v-model="formData.service_areas" placeholder="Enter your company name" type="text">
-                                        </div><!-- form-group -->
+                                    <div class="col-md-6 col-sm-12">
+                                      <div class="form-group">
+                                        <label>Tax Number</label>
+                                        <input class="form-control" v-model="formData.tax_number" placeholder="Enter your tax number" type="text">
+                                      </div>
+                                    </div><!-- col-md-6 col-sm-12 -->
+                                    <div class="col-md-6 col-sm-12">
+                                      <div class="form-group">
+                                        <label>Mobile</label>
+                                        <input class="form-control" v-model="formData.mobile" placeholder="Enter your phone number" type="text">
+                                      </div>
+                                    </div><!-- col-md-6 col-sm-12 -->
+                                    <div class="col-md-6 col-sm-12">
+                                      <div class="form-group">
+                                        <label>Phone</label>
+                                        <input class="form-control" v-model="formData.phone" placeholder="Enter your phone number" type="text">
+                                      </div>
+                                    </div><!-- col-md-6 col-sm-12 -->
+                                    <div class="col-md-6 col-sm-12">
+                                      <div class="form-group">
+                                        <label>Whatsapp</label>
+                                        <input class="form-control" v-model="formData.whatsapp" placeholder="Enter your phone number" type="text">
+                                      </div>
+                                    </div><!-- col-md-6 col-sm-12 -->
+                                    <div class="col-md-6 col-sm-12">
+                                      <div class="form-group">
+                                        <label>Company Name</label>
+                                        <input class="form-control" v-model="formData.company_name" placeholder="Enter your company name" type="text">
+                                      </div><!-- form-group -->
+                                    </div><!-- col-md-6 col-sm-12 -->
+                                    <div class="col-md-6 col-sm-12">
+                                      <div class="form-group">
+                                        <label>Service area</label>
+                                        <input class="form-control" v-model="formData.service_areas" placeholder="Enter your company name" type="text">
+                                      </div><!-- form-group -->
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-12 col-sm-12">
-                                        <div class="form-group">
-                                            <label>Address</label>
-                                            <textarea class="form-control" v-model="formData.address" rows="3" placeholder="Enter your address"></textarea>
-                                        </div><!-- form-group -->
+                                      <div class="form-group">
+                                        <label>Address</label>
+                                        <textarea class="form-control" v-model="formData.address" rows="3" placeholder="Enter your address"></textarea>
+                                      </div><!-- form-group -->
                                     </div><!-- col-md-6 col-sm-12 -->
                                     <div class="col-md-12 col-sm-12">
-                                        <div class="form-group">
-                                            <label>About me</label>
-                                            <textarea class="form-control" v-model="formData.about_me" rows="6" placeholder="Enter a description about you"></textarea>
-                                        </div><!-- form-group -->
+                                      <div class="form-group">
+                                        <label>About me</label>
+                                        <textarea class="form-control" v-model="formData.about_me" rows="6" placeholder="Enter a description about you"></textarea>
+                                      </div><!-- form-group -->
                                     </div><!-- col-md-6 col-sm-12 -->
-                                </div><!-- row -->
-                                <button class="btn btn-success">Update Profile</button>
+                                  </div><!-- row -->
+                                  <button class="btn btn-success" type="submit">Update Profile</button>
+                                </form>
                             </div><!-- col-md-9 col-sm-12 -->
                         </div><!-- row -->
                     </div><!-- dashboard-content-block -->
@@ -185,6 +186,18 @@ const handleFileChange = async (event) => {
     }
   }
 };
+
+const submitForm = async () => {
+  try {
+    const res = await profileToRefs.updateProfileInfo(formData.value);
+
+    if (res.status === 200){
+      console.log("updated success")
+    }
+  } catch (error) {
+    console.error("Failed to update profile picture:", error);
+  }
+}
 
 
 onMounted( () => {
