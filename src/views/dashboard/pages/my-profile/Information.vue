c139fab0eb14c2eecbfa1ac97dd2312543b2f2b0
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-01-21 00:19:27 +0500

    add validation on profile picture

--- src/views/dashboard/pages/my-profile/Information.vue
+++ src/views/dashboard/pages/my-profile/Information.vue
@@ -216,6 +216,23 @@ const triggerFileInput = () => {
 const handleFileChange = async (event) => {
   const file = event.target.files[0];
   if (file) {
+    const validExtensions = ['image/jpeg', 'image/png', 'image/jpg'];
+    const maxSizeInBytes = 2 * 1024 * 1024; // 2MB
+
+    // Check file type
+    if (!validExtensions.includes(file.type)) {
+      notify.Error("Invalid file type. Only JPG, JPEG, and PNG files are allowed.");
+      event.target.value = "";
+      return;
+    }
+
+    // Check file size
+    if (file.size > maxSizeInBytes) {
+      notify.Error("File size exceeds the 2MB limit. Please upload a smaller file.");
+      event.target.value = "";
+      return;
+    }
+
     const reader = new FileReader();
     reader.onload = (e) => {
       formProfilePicture.value = e.target.result;
@@ -238,6 +255,7 @@ const handleFileChange = async (event) => {
     }
   }
 };
+
 
 const submitForm = async () => {
   Object.keys(formData.value).forEach(validateField);
