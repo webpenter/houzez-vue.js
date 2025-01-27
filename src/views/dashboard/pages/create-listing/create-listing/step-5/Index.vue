bbe20afb54e6646e04b197b204bec33671746f45
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-01-28 02:41:49 +0500

    work on create listing and make 9 steps be dynamic

--- src/views/dashboard/pages/create-listing/create-listing/step-5/Index.vue
+++ src/views/dashboard/pages/create-listing/create-listing/step-5/Index.vue
@@ -5,15 +5,105 @@
         <section class="dashboard-content-wrap dashboard-add-new-listing">
             <snake-nav active="listing"/>
             <div class="dashboard-content-inner-wrap">
-                <Alert message="Settings saved successfully." type="success"/>
-                <Alert message="Something went wrong." type="danger"/>
+                <form @submit.prevent="formSubmit">
+                  <div class="dashboard-content-block-wrap">
+                    <h2>Property Location</h2>
+                      <div class="dashboard-content-block">
+                        <div class="row">
+                          <div class="col-md-6 col-sm-12">
+                            <div class="form-group">
+                              <label>Address</label>
+                              <input
+                                  class="form-control"
+                                  :class="{ 'is-invalid': localErrors.address }"
+                                  @input="validateField('address')"
+                                  v-model="formData.address"
+                                  placeholder="Enter your property address"
+                                  type="text"
+                                  autocomplete="off"
+                              >
+                              <span class="text-danger" v-if="localErrors.address">
+                                  {{ localErrors.address }}
+                              </span>
+                            </div>
+                          </div><!-- col-md-6 col-sm-12 -->
+                          <div class="col-md-6 col-sm-12">
+                            <div class="form-group">
+                              <label>Country</label>
+                              <input class="form-control" v-model="formData.country" placeholder="Enter your property country" type="text">
+                            </div>
+                          </div><!-- col-md-6 col-sm-12 -->
+                          <div class="col-md-6 col-sm-12">
+                            <div class="form-group">
+                              <label>County / State</label>
+                              <input class="form-control" v-model="formData.county_state" placeholder="Enter your property county/state" type="text">
+                            </div>
+                          </div><!-- col-md-6 col-sm-12 -->
+                          <div class="col-md-6 col-sm-12">
+                            <div class="form-group">
+                              <label>City</label>
+                              <input class="form-control" v-model="formData.city" placeholder="Enter your property city" type="text">
+                            </div>
+                          </div><!-- col-md-6 col-sm-12 -->
+                          <div class="col-md-6 col-sm-12">
+                            <div class="form-group">
+                              <label>Neighborhood</label>
+                              <input class="form-control" v-model="formData.neighborhood" placeholder="Enter your property neighborhood" type="text">
+                            </div>
+                          </div><!-- col-md-6 col-sm-12 -->
+                          <div class="col-md-6 col-sm-12">
+                            <div class="form-group">
+                              <label>Postal Code / Zip</label>
+                              <input class="form-control" v-model="formData.zip_postal_code" placeholder="Enter your property zip code" type="text">
+                            </div>
+                          </div><!-- col-md-6 col-sm-12 -->
+                        </div><!-- row -->
+                      </div><!-- dashboard-content-block -->
+                  </div><!-- dashboard-content-block-wrap -->
+                  <div class="dashboard-content-block-wrap">
+                    <h2>Map</h2>
+                    <div class="dashboard-content-block">
+                      <div class="row">
+                        <div class="col-md-12 col-sm-12">
+                          <div class="form-group">
+                            <label>Maps Latitude</label>
+                            <input
+                                class="form-control"
+                                :class="{ 'is-invalid': localErrors.latitude }"
+                                @input="validateField('latitude')"
+                                v-model="formData.latitude"
+                                placeholder="Enter google maps latitude"
+                                type="text"
+                            >
+                            <span class="text-danger" v-if="localErrors.latitude">
+                            {{ localErrors.latitude }}
+                          </span>
+                          </div>
+                          <div class="form-group">
+                            <label>Maps Longitude</label>
+                            <input
+                                class="form-control"
+                                :class="{ 'is-invalid': localErrors.longitude }"
+                                @input="validateField('longitude')"
+                                v-model="formData.longitude"
+                                placeholder="Enter google maps longitude"
+                                type="text"
+                            >
+                            <span class="text-danger" v-if="localErrors.longitude">
+                            {{ localErrors.longitude }}
+                           </span>
+                          </div>
+                        </div><!-- col-md-6 col-sm-12 -->
 
-                <SectionLocation/>
+                      </div><!-- row -->
+                    </div><!-- dashboard-content-block -->
+                  </div><!-- dashboard-content-block-wrap -->
 
-                <div class="d-flex justify-content-between add-new-listing-bottom-nav-wrap">
-                    <BackBtn route="dashboard.create-listing.step-4"/>  
-                    <NextBtn route="dashboard.create-listing.step-6"/>        
-                </div><!-- add-new-listing-bottom-nav-wrap -->
+                  <div class="d-flex justify-content-between add-new-listing-bottom-nav-wrap">
+                    <BackBtn route="dashboard.create-listing.step-4" :pId="propertyId"/>
+                    <NextBtn :btnLoading="btnLoading" :hasErrors="hasErrors"/>
+                  </div><!-- add-new-listing-bottom-nav-wrap -->
+                 </form>
             </div><!-- dashboard-content-inner-wrap -->
         </section><!-- dashboard-content-wrap -->
 </template>
@@ -24,4 +114,126 @@ import SaveAsDraftBtn from '../components/SaveAsDraftBtn.vue';
 import NextBtn from '../components/NextBtn.vue';
 import BackBtn from '../components/BackBtn.vue';
 import SectionLocation from '@/views/inc/dashboard/property/SectionLocation.vue';
+import Map from "@/views/inc/Map.vue";
+import {useRoute, useRouter} from "vue-router";
+import {useNotification, useProperty} from "@/stores/index.js";
+import {storeToRefs} from "pinia";
+import {computed, onMounted, ref, watch} from "vue";
+import {PROPERTY_TOTAL_STEPS} from "@/constants/index.js";
+
+const route = useRoute();
+const router = useRouter();
+const propertyId = route.params.propertyId;
+const propertyToRefs = useProperty();
+const {property} = storeToRefs(propertyToRefs);
+const notify = useNotification();
+const btnLoading = ref(false);
+
+const formData = ref({
+  address: "",
+  country: "",
+  county_state: "",
+  city: "",
+  neighborhood: "",
+  zip_postal_code: "",
+  map_street_view: "",
+  latitude: null,
+  longitude: null,
+});
+
+const localErrors = ref({
+  address: "",
+  latitude: "",
+  longitude: "",
+});
+
+const validateField = (field) => {
+  if (field === "address" && !formData.value.address) {
+    localErrors.value.address = "Address field is required.";
+  } else if (field === "latitude") {
+    if (formData.value.latitude && isNaN(formData.value.latitude)) {
+      localErrors.value.latitude = "Latitude field must be a number.";
+    } else if (
+        formData.value.latitude &&
+        (formData.value.latitude < -90 || formData.value.latitude > 90)
+    ) {
+      localErrors.value.latitude = "Latitude must be between -90 and 90.";
+    } else {
+      localErrors.value.latitude = "";
+    }
+  } else if (field === "longitude") {
+    if (formData.value.longitude && isNaN(formData.value.longitude)) {
+      localErrors.value.longitude = "Longitude field must be a number.";
+    } else if (
+        formData.value.longitude &&
+        (formData.value.longitude < -180 || formData.value.longitude > 180)
+    ) {
+      localErrors.value.longitude = "Longitude must be between -180 and 180.";
+    } else {
+      localErrors.value.longitude = "";
+    }
+  } else {
+    localErrors.value[field] = "";
+  }
+};
+
+
+const hasErrors = computed(() =>
+    Object.values(localErrors.value).some((error) => error !== "")
+);
+
+const editData = async () => {
+  const res = await propertyToRefs.edit(propertyId);
+
+  if (res.status === 404) {
+    return router.push({name:"property-not-found-404"});
+  } else if (res.status === 403) {
+    return router.push({name:"unauthorized-403"});
+  }
+}
+
+const formSubmit = async () => {
+  Object.keys(localErrors.value).forEach((field) => validateField(field));
+
+  if (hasErrors.value) {
+    notify.Error("Please fix the validation errors before proceeding.");
+    return;
+  }
+
+  btnLoading.value = true;
+
+  try {
+    const res = await propertyToRefs.createOrUpdate(formData.value, propertyId);
+
+    btnLoading.value = false;
+
+    if (res.status === 200) {
+      notify.Success(`Step 5 of ${PROPERTY_TOTAL_STEPS} completed. Your property has been recorded`);
+      router.push({name:"dashboard.create-listing.step-6",params:{propertyId:propertyId}});
+    } else if (res.status === 404) {
+      notify.Error("Property not found.");
+    } else if (res.status === 403) {
+      notify.Error("You are not authorized to perform this action.");
+    } else {
+      notify.Error("An error occurred while processing the request.");
+    }
+  } catch (error) {
+    btnLoading.value = false;
+    notify.Error("An error occurred");
+  }
+};
+
+onMounted(() => {
+  editData();
+
+  if (property.value){
+    formData.value = { ...property.value };
+  }
+});
+
+watch(property,(newProperty) => {
+  if (newProperty){
+    formData.value = { ...newProperty };
+  }
+});
 </script>
--- src/views/dashboard/pages/create-listing/create-listing/step-6/Index.vue
+++ src/views/dashboard/pages/create-listing/create-listing/step-6/Index.vue
@@ -5,15 +5,101 @@
         <section class="dashboard-content-wrap dashboard-add-new-listing">
             <snake-nav active="listing"/>
             <div class="dashboard-content-inner-wrap">
-                <Alert message="Settings saved successfully." type="success"/>
-                <Alert message="Something went wrong." type="danger"/>
 
-                <SectionMedia/>
+                <form @submit.prevent="formSubmit">
+                  <div class="dashboard-content-block-wrap">
+                    <h2>Property Media</h2>
+                    <div class="dashboard-content-block">
+                      <p>Drag and drop the images to customize the image gallery order.</p>
+                      <div class="upload-property-media">
+                        <div class="media-drag-drop">
+                          <div class="upload-icon">
+                            <i class="houzez-icon icon-picture-sun"></i>
+                          </div>
+                          <div class="upload-image-counter">3 of 6</div>
+                          <div>
+                            Drag and drop the gallery images here<br>
+                            <span>(Minimum size 1440x900)</span>
+                          </div>
+                          <button class="btn btn-primary btn-left-icon"><i class="houzez-icon icon-upload-button mr-1"></i> Select and Upload</button>
+                        </div>
+                        <p>Click on the star icon to select the cover image.</p>
+                        <div class="upload-media-gallery">
+                          <div class="row">
+                            <div class="col-md-2 col-sm-4 col-6">
+                              <img class="img-fluid" src="http://placehold.it/200x150" alt="thumb">
+                              <div class="upload-gallery-thumb-buttons">
+                                <button><i class="houzez-icon icon-rating-star full-star text-success"></i></button>
+                                <button><i class="houzez-icon icon-remove-circle"></i></button>
+                              </div>
+                            </div>
+                            <div class="col-md-2 col-sm-4 col-6">
+                              <img class="img-fluid" src="http://placehold.it/200x150" alt="thumb">
+                              <div class="upload-gallery-thumb-buttons">
+                                <button><i class="houzez-icon icon-rating-star full-star"></i></button>
+                                <button><i class="houzez-icon icon-remove-circle"></i></button>
+                              </div>
+                            </div>
+                            <div class="col-md-2 col-sm-4 col-6">
+                              <img class="img-fluid" src="http://placehold.it/200x150" alt="thumb">
+                              <div class="upload-gallery-thumb-buttons">
+                                <button><i class="houzez-icon icon-rating-star full-star"></i></button>
+                                <button><i class="houzez-icon icon-remove-circle"></i></button>
+                              </div>
+                            </div>
+                            <div class="col-md-2 col-sm-4 col-6">
+                              <img class="img-fluid" src="http://placehold.it/200x150" alt="thumb">
+                              <div class="upload-gallery-thumb-buttons">
+                                <button><i class="houzez-icon icon-rating-star full-star"></i></button>
+                                <button><i class="houzez-icon icon-remove-circle"></i></button>
+                              </div>
+                            </div>
+                            <div class="col-md-2 col-sm-4 col-6">
+                              <img class="img-fluid" src="http://placehold.it/200x150" alt="thumb">
+                              <div class="upload-gallery-thumb-buttons">
+                                <button><i class="houzez-icon icon-rating-star full-star"></i></button>
+                                <button><i class="houzez-icon icon-remove-circle"></i></button>
+                              </div>
+                            </div>
+                            <div class="col-md-2 col-sm-4 col-6">
+                              <img class="img-fluid" src="http://placehold.it/200x150" alt="thumb">
+                              <div class="upload-gallery-thumb-buttons">
+                                <button><i class="houzez-icon icon-rating-star full-star"></i></button>
+                                <button><i class="houzez-icon icon-remove-circle"></i></button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div><!-- dashboard-content-block -->
+                  </div><!-- dashboard-content-block-wrap -->
 
-                <div class="d-flex justify-content-between add-new-listing-bottom-nav-wrap">
-                    <BackBtn route="dashboard.create-listing.step-5"/>  
-                    <NextBtn route="dashboard.create-listing.step-7"/>        
-                </div><!-- add-new-listing-bottom-nav-wrap -->
+                  <div class="dashboard-content-block-wrap">
+                    <h2>Property Video</h2>
+                    <div class="dashboard-content-block">
+                      <div class="form-group">
+                        <label>Video URL</label>
+                        <input
+                            class="form-control"
+                            :class="{ 'is-invalid': localErrors.video_url }"
+                            @input="validateField('video_url')"
+                            v-model="formData.video_url"
+                            placeholder="YouTube, Vimeo, SWF File and MOV File are supported"
+                            type="text"
+                        >
+                        <span class="text-danger" v-if="localErrors.video_url">
+                            {{ localErrors.video_url }}
+                          </span>
+                        <small class="form-text text-muted">For example: https://www.youtube.com/watch?v=49d3Gn41IaA</small>
+                      </div>
+                    </div><!-- dashboard-content-block -->
+                  </div><!-- dashboard-content-block-wrap -->
+
+                  <div class="d-flex justify-content-between add-new-listing-bottom-nav-wrap">
+                    <BackBtn route="dashboard.create-listing.step-5" :pId="propertyId"/>
+                    <NextBtn :btnLoading="btnLoading" :hasErrors="hasErrors"/>
+                  </div><!-- add-new-listing-bottom-nav-wrap -->
+                </form>
             </div><!-- dashboard-content-inner-wrap -->
         </section><!-- dashboard-content-wrap -->
 </template>
@@ -24,4 +110,106 @@ import SaveAsDraftBtn from '../components/SaveAsDraftBtn.vue';
 import NextBtn from '../components/NextBtn.vue';
 import BackBtn from '../components/BackBtn.vue';
 import SectionMedia from '@/views/inc/dashboard/property/SectionMedia.vue';
+import {useRoute, useRouter} from "vue-router";
+import {useNotification, useProperty} from "@/stores/index.js";
+import {storeToRefs} from "pinia";
+import {computed, onMounted, ref, watch} from "vue";
+import {PROPERTY_TOTAL_STEPS} from "@/constants/index.js";
+
+const route = useRoute();
+const router = useRouter();
+const propertyId = route.params.propertyId;
+const propertyToRefs = useProperty();
+const {property} = storeToRefs(propertyToRefs);
+const notify = useNotification();
+const btnLoading = ref(false);
+
+const formData = ref({
+  video_url: "",
+});
+
+const localErrors = ref({
+  video_url: "",
+});
+
+const validateField = (field) => {
+  if (field === "video_url" && formData.value.video_url && !isValidUrl(formData.value.video_url)) {
+    localErrors.value.video_url = "Please enter a valid URL.";
+  }  else {
+    localErrors.value[field] = "";
+  }
+};
+
+const isValidUrl = (url) => {
+  const urlPattern = new RegExp(
+      "^(https?:\\/\\/)" + // protocol
+      "((([a-zA-Z0-9\\-]+\\.)+[a-zA-Z]{2,})|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR IPv4
+      "(\\:\\d+)?(\\/[-a-zA-Z0-9@:%._+~#=]*)*" + // port and path
+      "(\\?[;&a-zA-Z0-9@:%._+~#=]*)?" + // query string
+      "(\\#[-a-zA-Z0-9@:%._+~#=]*)?$", // fragment locator
+      "i"
+  );
+  return urlPattern.test(url);
+};
+
+
+const hasErrors = computed(() =>
+    Object.values(localErrors.value).some((error) => error !== "")
+);
+
+const editData = async () => {
+  const res = await propertyToRefs.edit(propertyId);
+
+  if (res.status === 404) {
+    return router.push({name:"property-not-found-404"});
+  } else if (res.status === 403) {
+    return router.push({name:"unauthorized-403"});
+  }
+}
+
+const formSubmit = async () => {
+  Object.keys(localErrors.value).forEach((field) => validateField(field));
+
+  if (hasErrors.value) {
+    notify.Error("Please fix the validation errors before proceeding.");
+    return;
+  }
+
+  btnLoading.value = true;
+
+  try {
+    const res = await propertyToRefs.createOrUpdate(formData.value, propertyId);
+
+    btnLoading.value = false;
+
+    if (res.status === 200) {
+      notify.Success(`Step 6 of ${PROPERTY_TOTAL_STEPS} completed. Your property has been recorded`);
+      router.push({name:"dashboard.create-listing.step-7",params:{propertyId:propertyId}});
+    } else if (res.status === 404) {
+      notify.Error("Property not found.");
+    } else if (res.status === 403) {
+      notify.Error("You are not authorized to perform this action.");
+    } else {
+      notify.Error("An error occurred while processing the request.");
+    }
+  } catch (error) {
+    btnLoading.value = false;
+    notify.Error("An error occurred");
+  }
+};
+
+onMounted(() => {
+  editData();
+
+  if (property.value){
+    formData.value = { ...property.value };
+  }
+});
+
+watch(property,(newProperty) => {
+  if (newProperty){
+    formData.value = { ...newProperty };
+  }
+});
 </script>
