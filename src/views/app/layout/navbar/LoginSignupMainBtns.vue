cdef50d51b1c3f2434f76f14a65e2828324ad35c
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-02-17 23:36:43 +0500

    set structure of nav

---
+++ src/views/app/layout/navbar/LoginSignupMainBtns.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    <p class=" btn-sm bg-none m-0" type="button" data-bs-toggle="dropdown"
+       aria-expanded="false">
+      <i class="fa-regular fa-user"></i>
+    </p>
+    <ul class="dropdown-menu bg-white position-absolute border-none">
+      <RouterLink :to="{name:'app.login'}">
+        <li>Login</li>
+      </RouterLink>
+      <RouterLink :to="{name:'app.register'}">
+        <li>Sign up</li>
+      </RouterLink>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import {RouterLink} from "vue-router";
+</script>
