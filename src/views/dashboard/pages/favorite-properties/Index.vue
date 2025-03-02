018397da14e57e755399450fea66fb30edf375ca
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-03-03 02:14:52 +0500

    favorite properties task done

--- src/views/dashboard/pages/favorite-properties/Index.vue
+++ src/views/dashboard/pages/favorite-properties/Index.vue
@@ -1,51 +1,71 @@
 <template>
-    <DashboardHeader heading="Favorite Properties">
-        <!-- <a class="btn btn-primary" href="#">View Public Profile</a> -->
-    </DashboardHeader>
-    <section class="dashboard-content-wrap">
-        <div class="dashboard-content-inner-wrap">
-            <div class="dashboard-content-block-wrap">
-                <div class="dashboard-content-block">
-                    You don't have any favorite properties yet!
-                </div><!-- dashboard-content-block -->
-                <table class="dashboard-table dashboard-table-properties table-lined responsive-table">
-                    <thead>
-                        <tr>
-                            <th>Thumbnail</th>
-                            <th>Address</th>
-                            <!-- <th>ID</th> -->
-                            <th>Type</th>
-                            <th>Status</th>
-                            <th>Price</th>
-                            <th>Featured</th>
-                            <!-- <th>Posted</th>
-                                <th>Expire</th> -->
-                            <th class="action-col">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
-                        <PropertyFavoriteTableItem />
+  <DashboardHeader heading="My Favorite Properties"/>
 
-                    </tbody>
-                </table><!-- dashboard-table -->
-                <Pagination />
-
-            </div><!-- dashboard-content-block-wrap -->
-        </div><!-- dashboard-content-inner-wrap -->
-    </section><!-- dashboard-content-wrap -->
+  <section class="dashboard-content-wrap">
+    <div class="dashboard-content-inner-wrap">
+      <div class="dashboard-content-block-wrap">
+        <NoDataMsg
+            v-if="properties.length < 1"
+            msg="You have no favorite properties yet."
+        />
+        <Table
+            v-else
+            :properties="properties"
+            :loading="loading"
+            @delete-property="(id) => deleteProperty(id)"
+        />
+      </div><!-- dashboard-content-block-wrap -->
+    </div><!-- dashboard-content-inner-wrap -->
+  </section><!-- dashboard-content-wrap -->
 </template>
 
 <script setup>
-import PropertyFavoriteTableItem from '@/views/inc/dashboard/property/PropertyFavoriteTableItem.vue';
-import Pagination from '@/views/inc/listing/Pagination.vue';
+import Table from './Table.vue';
+import { storeToRefs } from "pinia";
+import {onMounted, ref} from "vue";
+import {useConfirm, useFavoriteProperty, useMessage, useNotification} from "@/stores/index.js";
+import NoDataMsg from "@/views/dashboard/components/NoDataMsg.vue";
 
+const favoritePropertyStore = useFavoriteProperty();
+const { properties } = storeToRefs(favoritePropertyStore);
+
+const loading = ref(false);
+
+const getProperties = async () => {
+  loading.value = true;
+
+  try {
+    const res = await favoritePropertyStore.getUserFavoriteProperties();
+
+    if (res.status === 200) {
+      loading.value = false;
+    } else {
+      loading.value = false;
+      useNotification().Error("An error occurred while fetching data");
+    }
+  } catch (error) {
+    loading.value = false;
+    useNotification().Error("An error occurred");
+  }
+};
+
+const deleteProperty = async (id) => {
+  try {
+    await useConfirm().Warning("Are you sure you want to remove this property from favorites?");
+    const response = await favoritePropertyStore.deleteFavoriteProperty(id);
+
+    if (response.status === 200) {
+      useNotification().Success("Property removed successfully");
+      await getProperties();
+    }
+  } catch (error) {
+    if (error !== "cancel") {
+      useNotification().Error(error);
+    } else {
+      useMessage().Info("Request cancelled.");
+    }
+  }
+};
+
+onMounted(() => getProperties());
 </script>
