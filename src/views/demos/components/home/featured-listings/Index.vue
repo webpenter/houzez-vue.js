bd2acdde2ff7dc3a82c0ccf4990d528e797f7558
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-06-14 13:49:44 +0500

    set featured properties of home page

--- src/views/demos/components/home/featured-listings/Index.vue
+++ src/views/demos/components/home/featured-listings/Index.vue
@@ -1,37 +1,46 @@
 <template>
-    <section class="content-wrap">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2 style="margin: 100px 0 30px 0; text-align: center;">Discover Our Featured Properties</h2>
-                    <div class="property-cards-module property-cards-module-v1 property-cards-module-3-cols">
-                        <div class="listing-view grid-view card-deck grid-view-3-cols">
-                            <template v-if="loading">
-                                <PropertyCardSkeleton :loop="6" />
-                            </template>
-                            <template v-else>
-                                <ListItem
-                                    v-for="property in featuredProperties"
-                                    :key="property.id"
-                                    :property="property" 
-                                />
-                            </template>
-                        </div><!-- listing-view -->
-                        <div class="load-more-wrap">
-                            <a class="btn btn-primary-outlined btn-load-more" href="#">Load More</a>
-                        </div><!-- load-more-wrap -->
-                    </div><!-- property-grid-module -->
-                </div><!-- col-12 -->
-            </div><!-- row -->
-        </div><!-- container -->
-    </section><!-- listing-wrap -->
+<section class="content-wrap">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2 style="margin: 100px 0 30px 0; text-align: center;">Discover Our Featured Properties</h2>
+
+          <div class="property-cards-module property-cards-module-v1 property-cards-module-3-cols">
+            <div class="listing-view grid-view card-deck grid-view-3-cols">
+
+              <!-- Show skeletons while loading -->
+              <template v-if="loading">
+                <FeaturedPropertiesSkeleton v-for="n in 6" :key="'skeleton-' + n" />
+              </template>
+
+              <!-- Show actual properties after loading -->
+              <template v-else>
+                <ListItem
+                  v-for="property in featuredProperties"
+                  :key="property.id"
+                  :property="property"
+                />
+              </template>
+
+            </div><!-- listing-view -->
+
+            <!-- Load more button -->
+            <div class="load-more-wrap" v-if="!loading && featuredProperties.length">
+              <a class="btn btn-primary-outlined btn-load-more" href="#">Load More</a>
+            </div><!-- load-more-wrap -->
+
+          </div><!-- property-grid-module -->
+        </div><!-- col-12 -->
+      </div><!-- row -->
+    </div><!-- container -->
+  </section><!-- listing-wrap -->
 </template>
 
 <script setup>
 import { useAppProperty } from "@/stores/index.js";
 import { storeToRefs } from "pinia";
 import { onMounted, ref } from "vue";  
-import PropertyCardSkeleton from '@/components/skeleton/PropertyCardSkeleton.vue';
+import FeaturedPropertiesSkeleton from '@/components/skeleton/FeaturedPropertiesSkeleton.vue';
 import ListItem from './ListItem.vue';
 
 const propertyToRefs = useAppProperty();
