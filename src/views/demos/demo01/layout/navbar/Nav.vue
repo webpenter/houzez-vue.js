e21e46c7a1204ec1e2d7d69d3f39712e27843140
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-06-14 13:02:58 +0500

    set proper navigation and routes

--- src/views/demos/demo01/layout/navbar/Nav.vue
+++ src/views/demos/demo01/layout/navbar/Nav.vue
@@ -3,26 +3,68 @@
 	note: to add have the nav bar on the right add this class "justify-content-end" to "navbar-nav"
 	-------------------------------------------------------------------------------------------- -->	
 	<ul class="navbar-nav">
-		<li class="nav-item active">
-			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		<!-- <li class="nav-item active">
+			<router-link class="nav-link" :to="{name:'demo01'}">Home <span class="sr-only">(current)</span></router-link>
 		</li>
 		<li class="nav-item">
 			<a class="nav-link" href="#">Properties</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Item</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Item</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Item</a>
+		</li> -->
+		<li class="nav-item dropdown">
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Home</a>
+			<ul class="dropdown-menu">
+				<li class="nav-item">
+					<router-link class="dropdown-item" :to="{name:'app'}">Home (Default)</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link class="dropdown-item" :to="{name:'demo01'}">Home (Demo 01)</router-link>
+				</li>
+			</ul>
 		</li>
 		<li class="nav-item dropdown">
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Properties</a>
+			<ul class="dropdown-menu">
+				<li class="nav-item">
+					<router-link class="dropdown-item" :to="{name:'app.properties'}">Properties (Default)</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link class="dropdown-item" :to="{name:'demo01'}">Properties (Demo 01)</router-link>
+				</li>
+			</ul>
+		</li>
+		<li class="nav-item dropdown">
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Others</a>
+			<ul class="dropdown-menu">
+				<li class="nav-item">
+					<router-link class="dropdown-item" :to="{name:'app.about-us'}">About Us (Default)</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link class="dropdown-item" :to="{name:'demo01'}">About Us (Demo 01)</router-link>
+				</li>
+			</ul>
+		</li>
+		<li class="nav-item dropdown">
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+				{{locale === 'en' ? 'English' : 'Français'}}
+			</a>
+			<ul class="dropdown-menu">
+				<template v-for="currentLocale in availableLocales">
+					<li class="nav-item">
+						<a 
+							class="dropdown-item"
+							href="javascript:void(0);"
+            				@click="changeLang(currentLocale)"
+						>
+							{{currentLocale === 'en' ? 'English' : 'Français'}}
+						</a>
+					</li>
+				</template>
+			</ul>
+		</li>
+		<!-- <li class="nav-item dropdown">
 			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Dropdown</a>
 			<ul class="dropdown-menu">
 				<li class="nav-item">
-					<a class="dropdown-item" href="#">Action</a>
+					<router-link class="dropdown-item" :to="{name:'app'}">Default</router-link>
 				</li>
 				<li class="nav-item">
 					<a class="dropdown-item" href="#">Another action</a>
@@ -74,7 +116,7 @@
 					</ul>
 				</li>
 			</ul>
-		</li>
+		</li> -->
 		<!-- <li class="nav-item dropdown megamenu-item">
 			<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Megamenu</a>
             <MegaMenu />
@@ -82,7 +124,19 @@
 	</ul>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import MegaMenu from './MegaMenu.vue';
+import {useI18n} from "vue-i18n";
+import {useLanguage} from "@/stores/index.js";
+import { storeToRefs } from 'pinia';
 
+const { t, locale, availableLocales } = useI18n();
+const languageStore = useLanguage();
+const { locale: selectedLocale } = storeToRefs(languageStore);
+
+const changeLang = (lang) => {
+  selectedLocale.value = lang;
+  locale.value = lang;
+  languageStore.setLanguage(lang);
+};
 </script>
