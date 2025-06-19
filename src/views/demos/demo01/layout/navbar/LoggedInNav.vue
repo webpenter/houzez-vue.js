40ba386f5925ca8319459e9ca54a14b227aad394
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-06-20 00:39:17 +0500

    set profile routes and add binding

--- src/views/demos/demo01/layout/navbar/LoggedInNav.vue
+++ src/views/demos/demo01/layout/navbar/LoggedInNav.vue
@@ -11,37 +11,59 @@
 
       <!-- Create Listing Button (optional: protect this as well) -->
       <RouterLink class="btn btn-create-listing hidden-xs hidden-sm me-3" :to="{ name: 'dashboard' }">
-        Create a Listing
+        {{ $t('Create Listing') }}
       </RouterLink>
 
       <!-- Profile Section -->
-      <div class="navbar-logged-in-wrap navbar dropdown">
-        <a href="#" class="dropdown-toggle d-flex align-items-center" data-toggle="dropdown">
-          <img
-            v-if="token"
-            width="42"
-            height="42"
-            alt="author"
-            :src="profilePicture || 'https://placehold.co/42'"
-            class="rounded-circle"
-          />
-          <i v-else class="houzez-icon icon-single-neutral-circle fs-3"></i>
-        </a>
+       <template v-if="token">
+          <div class="navbar-logged-in-wrap navbar dropdown">
+            <a href="#" class="dropdown-toggle d-flex align-items-center" data-toggle="dropdown">
+              <img
+                width="42"
+                height="42"
+                alt="author"
+                :src="profilePicture || 'https://placehold.co/42'"
+                class="rounded-circle"
+              />
+            </a>
 
-        <!-- Dropdown only for logged in users -->
-        <ul v-if="token" class="logged-in-nav dropdown-menu dropdown-menu-end">
-          <li><router-link :to="{name:'dashboard.crm'}"><i class="houzez-icon icon-layout-dashboard me-2"></i>Board</router-link></li>
-          <li><router-link :to="{name:'dashboard.my-properties'}"><i class="houzez-icon icon-building-cloudy me-2"></i>Properties</router-link></li>
-          <li><router-link :to="{name:'dashboard.create-listing.step-1'}"><i class="houzez-icon icon-add-circle me-2"></i>Create Listing</router-link></li>
-          <li><router-link :to="{name:'dashboard.favorite-properties'}"><i class="houzez-icon icon-love-it me-2"></i>Favourites</router-link></li>
-          <li><router-link :to="{name:'dashboard.saved-searches'}"><i class="houzez-icon icon-search me-2"></i>Saved Searches</router-link></li>
-          <li><router-link :to="{name:'dashboard.messages'}"><i class="houzez-icon icon-messages-bubble me-2"></i>Messages</router-link></li>
-          <li><router-link :to="{name:'dashboard.subscriptions'}"><i class="houzez-icon icon-task-list-text-1 me-2"></i>Membership</router-link></li>
-          <li><router-link :to="{name:'dashboard.invoices'}"><i class="houzez-icon icon-accounting-document me-2"></i>Invoices</router-link></li>
-          <li><router-link :to="{name:'dashboard.my-profile'}"><i class="houzez-icon icon-single-neutral-circle me-2"></i>My Profile</router-link></li>
-          <li><a href="javascript:void(0)" @click.prevent="$filters.logout()"><i class="houzez-icon icon-lock-5 me-2"></i>Log Out</a></li>
-        </ul>
-      </div>
+            <!-- Dropdown only for logged in users -->
+            <ul class="logged-in-nav dropdown-menu dropdown-menu-end">
+              <li><router-link :to="{name:'dashboard.crm'}"><i class="houzez-icon icon-layout-dashboard me-2"></i>Board</router-link></li>
+              <li><router-link :to="{name:'dashboard.my-properties'}"><i class="houzez-icon icon-building-cloudy me-2"></i>Properties</router-link></li>
+              <li><router-link :to="{name:'dashboard.create-listing.step-1'}"><i class="houzez-icon icon-add-circle me-2"></i>Create Listing</router-link></li>
+              <li><router-link :to="{name:'dashboard.favorite-properties'}"><i class="houzez-icon icon-love-it me-2"></i>Favourites</router-link></li>
+              <li><router-link :to="{name:'dashboard.saved-searches'}"><i class="houzez-icon icon-search me-2"></i>Saved Searches</router-link></li>
+              <li><router-link :to="{name:'dashboard.messages'}"><i class="houzez-icon icon-messages-bubble me-2"></i>Messages</router-link></li>
+              <li><router-link :to="{name:'dashboard.subscriptions'}"><i class="houzez-icon icon-task-list-text-1 me-2"></i>Membership</router-link></li>
+              <li><router-link :to="{name:'dashboard.invoices'}"><i class="houzez-icon icon-accounting-document me-2"></i>Invoices</router-link></li>
+              <li><router-link :to="{name:'dashboard.my-profile'}"><i class="houzez-icon icon-single-neutral-circle me-2"></i>My Profile</router-link></li>
+              <li><a href="javascript:void(0)" @click.prevent="$filters.logout()"><i class="houzez-icon icon-lock-5 me-2"></i>Log Out</a></li>
+            </ul>
+          </div>
+       </template>
+
+       <template v-else>
+          <div class="navbar-logged-in-wrap navbar dropdown">
+            <a href="#" class="dropdown-toggle d-flex align-items-center" data-toggle="dropdown">
+              <i class="houzez-icon icon-single-neutral-circle fs-3"></i>
+            </a>
+
+            <!-- Dropdown only for logged in users -->
+            <ul class="logged-in-nav dropdown-menu dropdown-menu-end">
+              <li>
+                <router-link :to="{name:'auth.register'}">
+                  Auth / Sign Up
+                </router-link>
+              </li>
+              <li>
+                <router-link :to="{name:'auth.login'}">
+                  Auth / Sign In
+                </router-link>
+              </li>
+            </ul>
+        </div>
+       </template>
 
     </div>
   </nav>
--- src/views/demos/demo01/layout/navbar/Nav.vue
+++ src/views/demos/demo01/layout/navbar/Nav.vue
@@ -3,42 +3,54 @@
 	note: to add have the nav bar on the right add this class "justify-content-end" to "navbar-nav"
 	-------------------------------------------------------------------------------------------- -->	
 	<ul class="navbar-nav">
-		<!-- <li class="nav-item active">
-			<router-link class="nav-link" :to="{name:'demo01'}">Home <span class="sr-only">(current)</span></router-link>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Properties</a>
-		</li> -->
 		<li class="nav-item dropdown">
-			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Home</a>
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+				{{$t('Home')}}
+			</a>
 			<ul class="dropdown-menu">
 				<li class="nav-item">
-					<router-link class="dropdown-item" :to="{name:'app'}">Home (Default)</router-link>
+					<router-link class="dropdown-item" :to="{name:'demo01'}">
+						{{$t('Home')}} ({{ $t('Demo') }} 01)
+					</router-link>
 				</li>
 				<li class="nav-item">
-					<router-link class="dropdown-item" :to="{name:'demo01'}">Home (Demo 01)</router-link>
+					<router-link class="dropdown-item" :to="{name:'app'}">
+						{{$t('Home')}}
+					</router-link>
 				</li>
 			</ul>
 		</li>
 		<li class="nav-item dropdown">
-			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Properties</a>
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+				{{ $t('Properties') }}
+			</a>
 			<ul class="dropdown-menu">
 				<li class="nav-item">
-					<router-link class="dropdown-item" :to="{name:'app.properties'}">Properties (Default)</router-link>
+					<router-link class="dropdown-item" :to="{name:'app.properties'}">
+						{{ $t('Properties') }} ({{ $t('Demo') }} 01)
+					</router-link>
 				</li>
 				<li class="nav-item">
-					<router-link class="dropdown-item" :to="{name:'demo01'}">Properties (Demo 01)</router-link>
+					<router-link class="dropdown-item" :to="{name:'app.properties'}">
+						{{ $t('Properties') }}
+					</router-link>
 				</li>
 			</ul>
 		</li>
 		<li class="nav-item dropdown">
-			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Others</a>
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+				{{ $t('Others') }}
+			</a>
 			<ul class="dropdown-menu">
 				<li class="nav-item">
-					<router-link class="dropdown-item" :to="{name:'app.about-us'}">About Us (Default)</router-link>
+					<router-link class="dropdown-item" :to="{name:'app.about-us'}">
+						{{ $t('About Us') }} ({{ $t('Demo') }} 01)
+					</router-link>
 				</li>
 				<li class="nav-item">
-					<router-link class="dropdown-item" :to="{name:'demo01'}">About Us (Demo 01)</router-link>
+					<router-link class="dropdown-item" :to="{name:'app.about-us'}">
+						{{ $t('About Us') }}
+					</router-link>
 				</li>
 			</ul>
 		</li>
@@ -60,72 +72,10 @@
 				</template>
 			</ul>
 		</li>
-		<!-- <li class="nav-item dropdown">
-			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Dropdown</a>
-			<ul class="dropdown-menu">
-				<li class="nav-item">
-					<router-link class="dropdown-item" :to="{name:'app'}">Default</router-link>
-				</li>
-				<li class="nav-item">
-					<a class="dropdown-item" href="#">Another action</a>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="dropdown-item dropdown-toggle" data-toggle="dropdown" href="#">Submenu</a>
-					<ul class="dropdown-menu">
-						<li class="nav-item">
-							<a class="dropdown-item" href="#">Submenu action</a>
-						</li>
-						<li class="nav-item">
-							<a class="dropdown-item" href="#">Another submenu action</a>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="dropdown-item dropdown-toggle" href="#">Subsubmenu</a>
-							<ul class="dropdown-menu">
-								<li class="nav-item"><a class="dropdown-item" href="#">Subsubmenu action aa</a></li>
-								<li class="nav-item"><a class="dropdown-item" href="#">Another subsubmenu action</a></li>
-							</ul>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="dropdown-item dropdown-toggle" href="#">Second subsubmenu</a>
-							<ul class="dropdown-menu">
-								<li class="nav-item"><a class="dropdown-item" href="#">Subsubmenu action bb</a></li>
-								<li class="nav-item"><a class="dropdown-item" href="#">Another subsubmenu action</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li class="nav-item">
-					<a class="dropdown-item dropdown-toggle" href="#">Submenu 2</a>
-					<ul class="dropdown-menu">
-						<li class="nav-item"><a class="dropdown-item" href="#">Submenu action 2</a></li>
-						<li class="nav-item"><a class="dropdown-item" href="#">Another submenu action 2</a></li>
-
-
-						<li class="nav-item"><a class="dropdown-item dropdown-toggle" href="#">Subsubmenu</a>
-							<ul class="dropdown-menu">
-								<li class="nav-item"><a class="dropdown-item" href="#">Subsubmenu action 1 3</a></li>
-								<li class="nav-item"><a class="dropdown-item" href="#">Another subsubmenu action 2 3</a></li>
-							</ul>
-						</li>
-						<li class="nav-item"><a class="dropdown-item dropdown-toggle" href="#">Second subsubmenu 3</a>
-							<ul class="dropdown-menu">
-								<li class="nav-item"><a class="dropdown-item" href="#">Subsubmenu action 3 </a></li>
-								<li class="nav-item"><a class="dropdown-item" href="#">Another subsubmenu action 3</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</li> -->
-		<!-- <li class="nav-item dropdown megamenu-item">
-			<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Megamenu</a>
-            <MegaMenu />
-		</li> -->
 	</ul>
 </template>
 
 <script setup>
-import MegaMenu from './MegaMenu.vue';
 import {useI18n} from "vue-i18n";
 import {useLanguage} from "@/stores/index.js";
 import { storeToRefs } from 'pinia';
