516d15388d623757529157dcb4036ec954a15194
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-07-03 00:00:44 +0500

    set url path for production

--- src/views/demos/components/home/search-banner/HeroSection.vue
+++ src/views/demos/components/home/search-banner/HeroSection.vue
@@ -2,9 +2,9 @@
     <section class="top-banner-wrap position-relative overflow-hidden" style="height: 600px;">
       <!-- Banner background image -->
       <div
-        class="banner-inner position-absolute w-100 h-100"
-        style="background-image: url('/src/assets/img/app-side/miami-beach-1.jpg'); background-size: cover; background-position: center; z-index: 1;">
-      </div>
+  class="banner-inner position-absolute w-100 h-100"
+  :style="`background-image: url(${beachImg}); background-size: cover; background-position: center; z-index: 1;`">
+</div>
   
       <!-- Banner content -->
       <div class="banner-caption text-white text-center position-relative" style="z-index: 2; padding-top: 0px;">
@@ -19,4 +19,5 @@
   
   <script setup>
   import BannerSearch from './BannerSearch.vue';
+  import beachImg from '@/assets/img/app-side/miami-beach-1.jpg';
   </script>
