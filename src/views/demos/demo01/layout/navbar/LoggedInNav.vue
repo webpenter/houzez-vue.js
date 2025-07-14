fa82c4b2f110739ee3cfa97b9058f0384f83ccca
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2025-07-15 00:57:02 +0500

    localization of links done

--- src/views/demos/demo01/layout/navbar/LoggedInNav.vue
+++ src/views/demos/demo01/layout/navbar/LoggedInNav.vue
@@ -27,16 +27,16 @@
 
             <!-- Dropdown only for logged in users -->
             <ul class="logged-in-nav dropdown-menu dropdown-menu-end">
-              <li><router-link :to="{name:'dashboard.crm'}"><i class="houzez-icon icon-layout-dashboard me-2"></i>Board</router-link></li>
-              <li><router-link :to="{name:'dashboard.my-properties'}"><i class="houzez-icon icon-building-cloudy me-2"></i>Properties</router-link></li>
-              <li><router-link :to="{name:'dashboard.create-listing.step-1'}"><i class="houzez-icon icon-add-circle me-2"></i>Create Listing</router-link></li>
-              <li><router-link :to="{name:'dashboard.favorite-properties'}"><i class="houzez-icon icon-love-it me-2"></i>Favourites</router-link></li>
-              <li><router-link :to="{name:'dashboard.saved-searches'}"><i class="houzez-icon icon-search me-2"></i>Saved Searches</router-link></li>
-              <li><router-link :to="{name:'dashboard.messages'}"><i class="houzez-icon icon-messages-bubble me-2"></i>Messages</router-link></li>
-              <li><router-link :to="{name:'dashboard.subscriptions'}"><i class="houzez-icon icon-task-list-text-1 me-2"></i>Membership</router-link></li>
-              <li><router-link :to="{name:'dashboard.invoices'}"><i class="houzez-icon icon-accounting-document me-2"></i>Invoices</router-link></li>
-              <li><router-link :to="{name:'dashboard.my-profile'}"><i class="houzez-icon icon-single-neutral-circle me-2"></i>My Profile</router-link></li>
-              <li><a href="javascript:void(0)" @click.prevent="$filters.logout()"><i class="houzez-icon icon-lock-5 me-2"></i>Log Out</a></li>
+              <li><router-link :to="{name:'dashboard.crm'}"><i class="houzez-icon icon-layout-dashboard me-2"></i>{{ $t('Board') }}</router-link></li>
+              <li><router-link :to="{name:'dashboard.my-properties'}"><i class="houzez-icon icon-building-cloudy me-2"></i>{{ $t('Properties') }}</router-link></li>
+              <li><router-link :to="{name:'dashboard.create-listing.step-1'}"><i class="houzez-icon icon-add-circle me-2"></i>{{ $t('Create Listing') }}</router-link></li>
+              <li><router-link :to="{name:'dashboard.favorite-properties'}"><i class="houzez-icon icon-love-it me-2"></i>{{ $t('Favourites') }}</router-link></li>
+              <li><router-link :to="{name:'dashboard.saved-searches'}"><i class="houzez-icon icon-search me-2"></i>{{ $t('Saved Searches') }}</router-link></li>
+              <li><router-link :to="{name:'dashboard.messages'}"><i class="houzez-icon icon-messages-bubble me-2"></i>{{ $t('Messages') }}</router-link></li>
+              <li><router-link :to="{name:'dashboard.subscriptions'}"><i class="houzez-icon icon-task-list-text-1 me-2"></i>{{ $t('Membership') }}</router-link></li>
+              <li><router-link :to="{name:'dashboard.invoices'}"><i class="houzez-icon icon-accounting-document me-2"></i>{{ $t('Invoices') }}</router-link></li>
+              <li><router-link :to="{name:'dashboard.my-profile'}"><i class="houzez-icon icon-single-neutral-circle me-2"></i>{{ $t('My Profile') }}</router-link></li>
+              <li><a href="javascript:void(0)" @click.prevent="$filters.logout()"><i class="houzez-icon icon-lock-5 me-2"></i>{{ $t('Log Out') }}</a></li>
             </ul>
           </div>
        </template>
@@ -51,12 +51,12 @@
             <ul class="logged-in-nav dropdown-menu dropdown-menu-end">
               <li>
                 <router-link :to="{name:'auth.register'}">
-                  Auth / Sign Up
+                  {{ $t('Auth / Sign Up') }}
                 </router-link>
               </li>
               <li>
                 <router-link :to="{name:'auth.login'}">
-                  Auth / Sign In
+                  {{ $t('Auth / Sign In') }}
                 </router-link>
               </li>
             </ul>
